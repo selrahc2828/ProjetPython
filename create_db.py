fa2e1bfad939cd7d7826696aa5ac6818f20cc8b5
selrahc2828 <47382201+selrahc2828@users.noreply.github.com>
2020-07-24 12:55:58 +0200

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -8,3 +8,13 @@ cur.execute("CREATE TABLE codes (id VARCHAR(20) PRIMARY KEY NOT NULL, code TEXT 
 conn.commit()
 cur.close()
 conn.close()
+
+
+# create bdd users
+
+conn = connect("db/tab_users.db")
+cur = conn.cursor()
+cur.execute("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, ip_adress VARCHAR NOT NULL, naviguateur text, date TIMESTAMP)")
+conn.commit()
+cur.close()
+conn.close()
